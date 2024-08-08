90234a554eb9112c9ba925980ce92b3bd66b0685
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-08-08 22:00:27 +0900

    :zap: solving baekjoon-29720

---
+++ baekjoon/problem-29720.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+   "fmt"
+   "bufio"
+   "strings"
+   "os"
+   "strconv"
+)
+
+func main() {
+   reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+   nums := strings.Split(input, " ")
+   n, _ := strconv.Atoi(nums[0])
+   m, _ := strconv.Atoi(nums[1])
+   k, _ := strconv.Atoi(nums[2])
+   min := n-m*k
+   max := n-m*(k-1)-1
+   if min < 0 {
+      min = 0
+   }
+   if max < 0 {
+      max = 0
+   }
+   fmt.Printf("%v %v\n", min, max)
+ }
