5ee43267efc6cb82b48df43077b7876787c91c3e
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-08-08 21:48:30 +0900

    :zap: solving baekjoon-29863

---
+++ baekjoon/problem-29863.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+   "fmt"
+)
+
+func main() {
+   var x, y int
+   fmt.Scanf("%v", &x)
+   fmt.Scanf("%v", &y)
+   sum := 0
+   if x >= 20 {
+      sum = (24-x)
+      sum += y
+   }else {
+      sum = y-x
+   }
+   fmt.Println(sum)
+}
