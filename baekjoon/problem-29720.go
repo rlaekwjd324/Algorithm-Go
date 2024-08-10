8cb2ea66ef3aad2b9d23d10b81558559bdeb4efa
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-08-10 13:07:18 +0900

    :tada solve baekjoon-29720

--- baekjoon/problem-29720.go
+++ baekjoon/problem-29720.go
@@ -10,8 +10,8 @@ import (
 
 func main() {
    reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	input = strings.TrimSpace(input)
+   input, _ := reader.ReadString('\n')
+   input = strings.TrimSpace(input)
    nums := strings.Split(input, " ")
    n, _ := strconv.Atoi(nums[0])
    m, _ := strconv.Atoi(nums[1])
