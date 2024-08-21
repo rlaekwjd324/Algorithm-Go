9d4cf6ba5cc22765d7764fd1d18c2a4c667dd5f9
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-08-21 21:37:30 +0900

    Create problem-32132.go

---
+++ baekjoon/problem-32132.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+   "fmt"
+   "strings"
+)
+
+func main() {
+   var n int
+   fmt.Scanln(&n)
+   var str string
+   fmt.Scanln(&str)
+   for {
+      if strings.Contains(str, "PS4") {
+         index := strings.Index(str, "PS4")
+         str = str[:index+2]+str[index+3:]
+      } else if strings.Contains(str, "PS5") {
+         index := strings.Index(str, "PS5")
+         str = str[:index+2]+str[index+3:]
+      } else {
+         break
+      }
+   } 
+   fmt.Println(str)
+}
