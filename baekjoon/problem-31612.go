3f15ba2d6648dde756708a96380eb8a562af897e
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-08-29 22:45:25 +0900

    :sparkles: create baekjoon-31612

---
+++ baekjoon/problem-31612.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+   "fmt"
+   "strings"
+)
+
+func main() {
+   var t int
+   fmt.Scanf("%v", &t)
+   var word string
+   fmt.Scanf("%v", &word)
+   word = strings.TrimSpace(word)
+   wordArr := strings.Split(word, "")
+   var sum = 0
+   for i := 0; i < t; i++ {
+      if wordArr[i] == "o"{
+         sum += 1
+      }else {
+         sum += 2
+      }
+   }
+   fmt.Println(sum)
+}
