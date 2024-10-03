8756d8b079f834779b550a2c063ad33e3e60c61d
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-10-03 22:18:26 +0900

    :sparkles: add baekjoon-2857

---
+++ baekjoon/problem-2857.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+   "bufio"
+	"fmt"
+	"os"
+   "strings"
+   "strconv"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+   defer writer.Flush()
+
+   arr := []string{}
+   for i := 0; i < 5; i++ {
+      var n string
+      fmt.Fscan(reader, &n)
+      
+      if strings.Contains(n, "FBI") {
+         index := strconv.Itoa(i+1)
+         arr = append(arr, index)
+      }
+   }
+   if len(arr) == 0 {
+      fmt.Fprintln(writer, "HE GOT AWAY!")
+      return
+   }
+   fmt.Fprintln(writer, strings.Join(arr, " "))
+}
