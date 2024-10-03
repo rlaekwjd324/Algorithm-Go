1c8a773ae81cb15110fe22dd9ffd1b3fecc26114
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-10-03 22:37:23 +0900

    :sparkles: add baekjoon-3449

---
+++ baekjoon/problem-3449.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+   "bufio"
+	"fmt"
+	"os"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+   defer writer.Flush()
+
+   var t int
+   fmt.Fscan(reader, &t)
+
+   for i := 0; i < t; i++ {
+      var a, b string
+      fmt.Fscan(reader, &a, &b)
+      count := 0
+      for i := 0; i < len(a); i++ {
+         if a[i] != b[i] {
+            count++
+         }
+      }
+      fmt.Fprintln(writer, fmt.Sprintf("Hamming distance is %v.", count))
+   }
+}
