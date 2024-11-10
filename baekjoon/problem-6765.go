a0cbe818b3a7e16d1b623e3c9989715f1cf439f9
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-11-10 21:29:01 +0900

    :tada: solve baekjoon-6765

---
+++ baekjoon/problem-6765.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+   "bufio"
+	"fmt"
+	"os"
+   "strings"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+   defer writer.Flush()
+
+   var n int
+   fmt.Fscanln(reader, &n)
+   
+   answer := ""
+
+   for i := 0; i < 3; i++ {
+      line := ""
+      if i == 0 {
+         line += strings.Repeat("*", n)
+         line += strings.Repeat("X", n)
+         line += strings.Repeat("*", n)
+      }else if i == 1 {
+         line += strings.Repeat(" ", n)
+         line += strings.Repeat("X", n)
+         line += strings.Repeat("X", n)
+      }else if i == 2 {
+         line += strings.Repeat("*", n)
+         line += strings.Repeat(" ", n)
+         line += strings.Repeat("*", n)
+      }
+      answer += strings.Repeat(line+"\n", n)
+   }
+   fmt.Fprintln(writer, answer)
+}
