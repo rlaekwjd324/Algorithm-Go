3717933a7d57f0d7919e5911ff18f9b15c7e3f5f
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-12-03 00:00:48 +0900

    :tada: solve baekjoon-32762

--- baekjoon/problem-32762.go
+++ baekjoon/problem-32762.go
@@ -15,17 +15,32 @@ func main() {
    var n, m int
    fmt.Fscan(reader, &n, &m)
 
+   sArr := make([]int, n)
+   eArr := make([]int, n)
    for i := 0; i < n; i++ {
       var s, e int
       fmt.Fscan(reader, &s, &e)
-
+      sArr[i] = s
+      eArr[i] = e
    }
+   sum := 0
    for i := 0; i < m; i++ {
       var t, p int
       fmt.Fscan(reader, &t, &p)
 
+      if isExist(sArr, eArr, t) {
+         sum += p
+      }
    }
    
-   
-   fmt.Fprintln(writer, "")
+   fmt.Fprintf(writer, "%.4f\n", float64(sum)/float64(n))
+}
+
+func isExist(s, e []int, t int) bool {
+   for i := 0; i < len(s); i++ {
+      if t >= s[i] && t <= e[i] {
+         return true
+      }
+   }
+   return false
 }
