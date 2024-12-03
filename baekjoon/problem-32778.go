99fb1279fd055dff2923e21e713994ef9277f60a
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-12-03 21:42:47 +0900

    :sparkles: add baekjoon-32778

---
+++ baekjoon/problem-32778.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+   "bufio"
+	"fmt"
+	"os"
+   "strings"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+   defer writer.Flush()
+
+   n, err := reader.ReadString('\n')
+   if err != nil {
+		return
+	}
+
+   if strings.Contains(n, "(") {
+      arr := strings.Split(n, " (")
+      fmt.Fprintln(writer, arr[0])
+      m := strings.Split(arr[1], ")")[0]
+      fmt.Fprintln(writer, m)
+   } else {
+      fmt.Fprint(writer, n)
+      fmt.Fprintln(writer, "-")
+   }
+}
