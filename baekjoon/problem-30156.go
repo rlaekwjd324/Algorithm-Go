5cf8e0764caf2b3f3d45d0e0e715553fc7fce650
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2024-12-27 13:46:29 +0900

    :tada: solve baekjoon-30156

--- baekjoon/problem-30156.go
+++ baekjoon/problem-30156.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-   "bufio"
-	 "fmt"
-	 "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 var reader *bufio.Reader = bufio.NewReader(os.Stdin)
 var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
 
 func main() {
-   defer writer.Flush()
+	defer writer.Flush()
 
-   var t int
-   fmt.Fscan(reader, &t)
+	var t int
+	fmt.Fscan(reader, &t)
 
-   for i := 0; i < t; i++ {
-      var w string
-      fmt.Fscan(reader, &w)
-      
-      c := 0
-      for _, v := range w {
-         if v == 'a' {
-            c++
-         }
-      }
-      if c >= len(w)/2 {
-         fmt.Fprintln(writer, len(w)-c)
-      } else {
-         fmt.Fprintln(writer, c)
-      }
-   }
+	for i := 0; i < t; i++ {
+		var w string
+		fmt.Fscan(reader, &w)
+
+		c := 0
+		for _, v := range w {
+			if v == 'a' {
+				c++
+			}
+		}
+		if c > len(w)-c {
+			fmt.Fprintln(writer, len(w)-c)
+		} else {
+			fmt.Fprintln(writer, c)
+		}
+	}
 }
