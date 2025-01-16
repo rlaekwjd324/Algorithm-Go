70b133d45aa6f84c1d8b7c27b55cb555dd5fc634
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2025-01-17 07:52:10 +0900

    :tada: solve baekjoon-33161

---
+++ baekjoon/problem-33161.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+   "bufio"
+   "fmt"
+   "os"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+   defer writer.Flush()
+
+   var n int
+   fmt.Fscan(reader, &n)
+   
+   fmt.Fprintln(writer, n/5)
+}
