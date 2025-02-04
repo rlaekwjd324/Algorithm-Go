53ab2b7e428518f4073a51cb6c1b11325f6dcda6
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2025-02-05 00:22:41 +0900

    :tada: solve baekjoon-33246

--- baekjoon/problem-33246.go
+++ baekjoon/problem-33246.go
@@ -20,10 +20,30 @@ func main() {
       var s, d string
       fmt.Fscan(reader, &s, &d)
       
-      if d == "ALLOWED" && s != p {
-         fmt.Fprintln(writer, "INTEGRITY OVERFLOW")
-         return
+      if d == "ALLOWED" {
+         if len(s) != len(p) || !isValid(p, s) {
+            fmt.Fprintln(writer, "INTEGRITY OVERFLOW")
+            return
+         }
+      }
+      if d == "DENIED"  && s == p {
+            fmt.Fprintln(writer, "INTEGRITY OVERFLOW")
+            return
       }
    }
+
    fmt.Fprintln(writer, "SYSTEM SECURE")
+}
+
+func isValid(p, s string) bool {
+   c := 0
+   for i := 0; i < len(p); i++ {
+      if p[i] != s[i] {
+         c++
+      }
+      if c > 1 {
+         return false
+      }
+   }
+   return true
 }
