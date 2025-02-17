b7e1ed03b83c2fb4fc7bfc31f113baa14d31eaa2
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2025-02-18 00:28:51 +0900

    :tada: solve baekjoon-31495

--- baekjoon/problem-31495.go
+++ baekjoon/problem-31495.go
@@ -6,22 +6,24 @@ import (
    "os"
 )
 
-var reader *bufio.Reader = bufio.NewReader(os.Stdin)
 var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
 
 func main() {
    defer writer.Flush()
 
-   var s string
-   fmt.Fscan(reader, &s)
+   scanner := bufio.NewScanner(os.Stdin)
+
+	// 한 줄 입력받기
+	scanner.Scan()
+	s := scanner.Text()
 
    if len(s) <= 2 {
      fmt.Fprintln(writer, "CE")
      return
    }
-   for i, v := range s {
+   for i, _ := range s {
      if i == 0 || i == len(s) - 1 {
-         if v != '"' {
+         if string(s[i]) != "\"" {
              fmt.Fprintln(writer, "CE")
              return
          }
