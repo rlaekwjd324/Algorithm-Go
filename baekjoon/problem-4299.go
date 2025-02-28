a827059453f0c27d8daa52f4fa6aff48985e6ccb
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2025-02-28 23:22:09 +0900

    :tada: baekjoon-4299

--- baekjoon/problem-4299.go
+++ baekjoon/problem-4299.go
@@ -15,17 +15,17 @@ func main() {
    // a+b a-b
    var sum, min int
    fmt.Fscan(reader, &sum, &min)
-   
-   if sum+min % 2 != 0 {
+
+   if (sum+min) % 2 != 0 {
       fmt.Fprintf(writer, "%v\n", -1)
       return
    }
    a := (sum+min)/2
    b := sum - a
-   if b <= 0 {
+   if b < 0 {
       fmt.Fprintf(writer, "%v\n", -1)
       return
    }
    
-   fmt.Fprintf(writer, "%v\n%v\n", a, b)
+   fmt.Fprintf(writer, "%v %v\n", a, b)
 }
