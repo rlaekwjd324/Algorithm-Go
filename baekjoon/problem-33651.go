1322acbdfa007863ce43ac600529d67e0bf1f4a6
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2025-04-08 08:40:26 +0900

    :sparkles: add baekjoon-33651

---
+++ baekjoon/problem-33651.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+   "bufio"
+   "fmt"
+   "os"
+   "strings"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+   defer writer.Flush()
+   
+   var s string
+   fmt.Fscan(reader, &s)
+   str := "UAPC"
+   a := ""
+   for _, v := range str {
+      if !strings.Contains(s, string(v)){
+         a += string(v)
+      }
+   }
+   fmt.Fprintln(writer, a)
+}
