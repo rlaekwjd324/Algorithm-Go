0e90b3dc3a23c4464994f79f69f1ab616997421b
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2025-05-25 00:06:48 +0900

    :tada: solve baekjoon-33909

--- baekjoon/problem-33909.go
+++ baekjoon/problem-33909.go
@@ -15,7 +15,38 @@ func main() {
    var s, c, o, n int
    fmt.Fscan(reader, &s, &c, &o, &n)
    
-   
+   a := 0
+   for {
+      if s >= 2 {
+         s -= 2
+      } else if n >= 2 {
+         n -= 2
+      } else {
+         break
+      }
+      if c >= 4 {
+         c -= 4
+      } else if o >= 2 {
+         o -= 2
+      } else {
+         break
+      }
+      if o >= 1 {
+         o -= 1
+      } else if c >= 2 {
+         c -= 2
+      } else {
+         break
+      }
+      if n >= 1 {
+         n -= 1
+      } else if s >= 1 {
+         s -= 1
+      } else {
+         break
+      }
+      a++
+   }
 
-   fmt.Fprintf(writer, "%v\n", "")
+   fmt.Fprintf(writer, "%v\n", a)
 }
