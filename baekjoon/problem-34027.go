4b2ff0c26cc8272dfa272b2f61005adcc6195109
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2025-06-19 19:48:35 +0900

    :sparkles: add baekjoon-34027

---
+++ baekjoon/problem-34027.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+   "bufio"
+   "fmt"
+   "os"
+   "math"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+   defer writer.Flush()
+
+   var n int
+   fmt.Fscan(reader, &n)
+   
+   for i := 0; i < n; i++ {
+      var a float64
+      fmt.Fscan(reader, &a)
+      
+      if math.Sqrt(a)-float64(int(math.Sqrt(a))) == 0.0 {
+         fmt.Fprintf(writer, "%v\n", 1)
+         continue
+      }
+      fmt.Fprintf(writer, "%v\n", 0)
+   }
+}
