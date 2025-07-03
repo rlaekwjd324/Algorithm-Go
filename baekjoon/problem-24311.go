a45491e602a45b66af81e2120583b82a43262f05
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2025-07-03 23:04:42 +0900

    :sparkles: add baekjoon-24311

---
+++ baekjoon/problem-24311.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+   "bufio"
+   "fmt"
+   "os"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+   defer writer.Flush()
+
+   var t1, t2, t3, t4, t5, t6, br int
+   fmt.Fscan(reader, &t1, &t2, &t3, &t4, &t5, &br, &t6)
+
+   h := 0
+   m := 10
+   m += t3
+   h += t4
+   m += t5
+   m += (br+1)*t6
+   
+   h = t1 - h
+   m = t2 - m
+   for {
+      if m >= 0 {
+         break
+      }
+      m += 60
+      h -= 1
+   }
+   
+   if h < 10 {
+      fmt.Fprintf(writer, "0%v ", h)
+   } else {
+      fmt.Fprintf(writer, "%v ", h)
+   }
+   if m < 10 {
+      fmt.Fprintf(writer, "0%v\n", m)
+      return
+   }
+   fmt.Fprintf(writer, "%v\n", m)
+}
