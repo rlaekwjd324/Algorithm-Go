81ee0d8fadc49d0c07fc88a9c247338169c06e69
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2025-07-06 20:00:02 +0900

    :sparkles: add baekjoon-27272

---
+++ baekjoon/problem-27272.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+   "bufio"
+   "fmt"
+   "os"
+   "sort"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+   defer writer.Flush()
+
+   arr := []int{}
+   for i := 0; i < 4; i++ {
+      var a int
+      fmt.Fscan(reader, &a)
+      arr = append(arr, a)
+   }
+
+   sort.Ints(arr)
+
+   fmt.Fprintf(writer, "%v\n", arr[0]*arr[1]+arr[2]*arr[3])
+}
