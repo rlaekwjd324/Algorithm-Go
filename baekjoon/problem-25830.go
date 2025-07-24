c1c74e95e6c555a132a9f09533bbd23bee90229c
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2025-07-24 21:26:42 +0900

    :sparkles: add baekjoon-25830

---
+++ baekjoon/problem-25830.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+   "bufio"
+   "fmt"
+   "os"
+   "strconv"
+   "strings"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+   defer writer.Flush()
+
+   var n string
+   fmt.Fscan(reader, &n)
+
+   mm, _ := strconv.Atoi(strings.Split(n, ":")[0])
+   ss, _ := strconv.Atoi(strings.Split(n, ":")[1])
+   s := 0-ss
+   m := ss-mm
+   h := mm
+   if s < 0 {
+      s+=60
+      m--
+   }
+   if m < 0 {
+      m+=60
+      h--
+   }
+
+   nh := fmt.Sprintf("%v", h)
+   nm := fmt.Sprintf("%v", m)
+   ns := fmt.Sprintf("%v", s)
+   if h < 10 {
+      nh = "0"+nh
+   }
+   if m < 10 {
+      nm = "0"+nm
+   }
+   if s < 10 {
+      ns = "0"+ns
+   }
+   
+   fmt.Fprintf(writer, "%v:%v:%v\n", nh, nm, ns)
+}
