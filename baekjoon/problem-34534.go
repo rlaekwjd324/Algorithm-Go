6a1c841697ae8a233f7f12a74bbf5d9461830fe3
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2025-10-01 23:49:00 +0900

    :sparkles: add baekjoon-34534

---
+++ baekjoon/problem-34534.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+   "strings"
+   "strconv"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+
+   cards := ""
+   for i := 0; i < 5; i++ {
+      var a string
+      fmt.Fscan(reader, &a)
+
+      cards += (a+" ")
+   }
+
+   min := 5
+   for i := 1; i <= 5; i++ {
+      s := 0
+      for j := 0; j < 5; j++ {
+         if !strings.Contains(cards, strconv.Itoa(i+j)) {
+            s++
+         }
+      }
+      if s < min {
+         min = s
+      }
+   }
+   
+   fmt.Fprintf(writer, "%v\n", min)
+}
