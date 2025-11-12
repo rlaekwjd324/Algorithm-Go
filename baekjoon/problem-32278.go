ed2c04c267f220ef8165457b096825a23e9a7e97
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2025-11-12 22:41:10 +0900

    :sparkles: add baekjoon-32278

---
+++ baekjoon/problem-32278.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+	"strconv"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+    defer writer.Flush()
+
+    var n string
+    fmt.Fscan(reader, &n)
+	
+	if string(n[0]) == "-" {
+		n = n[1:]
+	}
+
+	if len(n) < 6 {
+		ni, _ := strconv.Atoi(n)
+		if ni < 32768 {
+			fmt.Fprintf(writer, "%v\n", "short")
+			return
+		}
+	}
+	
+	if len(n) < 11 {
+		ni, _ := strconv.Atoi(n)
+		if ni < 2147483649 {
+			fmt.Fprintf(writer, "%v\n", "int")
+			return
+		}
+	}
+
+	fmt.Fprintf(writer, "%v\n", "long long")
+}
