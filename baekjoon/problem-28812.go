02728697fecce66fbad19f698ae3e86dd6bb2330
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2025-12-17 20:21:49 +0900

    :sparkles: add baekjoon-28812

---
+++ baekjoon/problem-28812.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+    defer writer.Flush()
+
+	var n, c int
+	fmt.Fscan(reader, &n, &c)
+	
+	min := 10201
+	for i := 0; i < n; i++ {
+		var p, d, v int
+		fmt.Fscan(reader, &p, &d, &v)
+		
+		t := p+d+v*c
+		if min > t {
+			min = t
+		}
+	}
+
+	fmt.Fprintf(writer, "%v\n", min)
+}
