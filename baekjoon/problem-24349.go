c5ded6d02108b4a6d035c147932bd9487fddb6a3
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2026-03-17 00:04:20 +0900

    :tada: solve baekjoon-24349

--- baekjoon/problem-24349.go
+++ baekjoon/problem-24349.go
@@ -1,1 +1,35 @@
+package main
 
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+    defer writer.Flush()
+
+	var n, a, b, c int
+	fmt.Fscan(reader, &n, &a, &b, &c)
+
+	d := 0
+	if n == 1 {
+		d = 0
+	} else {
+		d = min(a, b) + (n - 2) * min(min(a, b), c)
+	}
+
+	h := d/100
+	d = d%100
+	fmt.Fprintf(writer, "%v %v\n", h, d)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
