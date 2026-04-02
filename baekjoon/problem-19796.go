b865b36d82faa94b167a0bca6794104a101cb288
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2026-04-02 23:31:24 +0900

    :sparkles: add baekjoon-19796

---
+++ baekjoon/problem-19796.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+    defer writer.Flush()
+
+	var m, n int
+	fmt.Fscan(reader, &m, &n)
+	
+	arr := make([]bool, m)
+	for i := 0; i < n; i++ {
+		var l, r int
+		fmt.Fscan(reader, &l, &r)
+		
+		for j := l-1; j < r; j++ {
+			arr[j] = true
+		}
+	}
+
+	for _, v := range arr {
+		if !v {
+			fmt.Fprintf(writer, "%v\n", "NO")
+			return
+		}
+	}
+	fmt.Fprintf(writer, "%v\n", "YES")
+}
