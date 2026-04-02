2cf891f6f6c310894164a973ca82fc02ab9c24d9
김다정 (도리) <54465767+rlaekwjd324@users.noreply.github.com>
2026-04-02 23:37:18 +0900

    :sparkles: add baekjoon-7218

---
+++ baekjoon/problem-7218.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+	"strings"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+    defer writer.Flush()
+
+	var n int
+	var s string
+	fmt.Fscan(reader, &n, &s)
+
+	arr := []string{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"}
+	
+	for i, v := range arr {
+		if strings.Contains(s, v) {
+			fmt.Fprintf(writer, "%v ", i+1)
+		}
+	}
+}
